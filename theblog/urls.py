2187227e0f788796f3f4c912ab37d6bec0378a00
deki90to <deki90to@gmail.com>
2020-04-23 16:29:28 +0200

    upload BLogPost

---
+++ theblog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+# from . import views
+from . views import HomeView, articleDetailView, AddPostView, UpdatePostView, DeletePostView
+
+
+urlpatterns = [
+	# path('',views.home, name='home')
+	path('', HomeView.as_view(), name="home"),
+	path('article/<int:pk>', articleDetailView.as_view(), name='article_details'),
+	path('add_post/', AddPostView.as_view(), name='add_post'),
+	path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+	path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+]
