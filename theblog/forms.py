c399b2e519e33721a356c406bc504c9ae6ceef7b
deki90to <deki90to@gmail.com>
2020-05-09 00:54:45 +0200

    hidding selecting users

--- theblog/forms.py
+++ theblog/forms.py
@@ -12,6 +12,7 @@ class PostForm(forms.ModelForm):
 		widgets = {
 			# 'title':forms.TextInput(attrs={'class': 'form-control'}),
 			# 'title_tag':forms.TextInput(attrs={'class': 'form-control'}),
-			'author':forms.Select(attrs={'class': 'form-control'}),
-			'body':forms.Textarea(attrs={'class': 'form-control'}),
+			# 'author':forms.Select(attrs={'class': 'form-control'}),
+			'author':forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'admin', 'type':'hidden'}),
+			'body':forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Type here...'}),
 		} 
