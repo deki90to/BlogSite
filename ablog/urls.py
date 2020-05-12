1e88617aed87abe60b27de2ef10d6bebba4805f6
deki90to <deki90to@gmail.com>
2020-05-12 21:26:37 +0200

    font

--- ablog/urls.py
+++ ablog/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -10,3 +12,6 @@ urlpatterns = [
     path('members/', include('django.contrib.auth.urls')),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
